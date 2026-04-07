da80c055fc3bfb10e83ecbce0a3e32bf2a8302ff
Nate_Theodore <natnael.welduu@gmail.com.github.com>
2026-04-07 13:51:45 +0300

    Fix import statements for generator and schema modules

--- back-end/main.py
+++ back-end/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from generator import ask_gubae
-from schema import ChatRequest
+from .generator import ask_gubae
+from .schema import ChatRequest
 
 app = FastAPI()
 
